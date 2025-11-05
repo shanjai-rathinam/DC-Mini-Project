371e6f6752112cf5f5b9696987487b73def3e430
Shanjai Rathinam <rathinamshanjai@gmail.com>
2025-11-05 11:47:37 +0530

    Add Vote class to handle voting functionality

--- voting/vote.py
+++ voting/vote.py
@@ -1,1 +1,12 @@
+# voting/vote.py
 
+import time
+
+class Vote:
+    def __init__(self, voter_id, candidate_id):
+        self.voter_id = voter_id
+        self.candidate_id = candidate_id
+        self.timestamp = time.time()
+
+    def to_dict(self):
+        return self.__dict__
