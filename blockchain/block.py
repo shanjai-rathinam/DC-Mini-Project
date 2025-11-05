5de50ba748648b83851fe09d4f84b694cc24dec0
Shanjai Rathinam <rathinamshanjai@gmail.com>
2025-11-05 11:46:52 +0530

    Implement Block class for blockchain structure
    Added Block class with initialization and hash computation.

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,1 +1,18 @@
+# blockchain/block.py
 
+import time
+from utils.cryptography import hash_data
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        self.hash = self.compute_hash()
+
+    def compute_hash(self):
+        """Computes the hash of the block."""
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return hash_data(block_string)
